64cc5327272a418d4d4546510162f9de17034c32
Crowdbotics <team@crowdbotics.com>
2019-04-08 14:04:56 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.googlevision_2076.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/googlevision_2076/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for googlevision_2076 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "googlevision_2076.settings")
+
+application = get_wsgi_application()
